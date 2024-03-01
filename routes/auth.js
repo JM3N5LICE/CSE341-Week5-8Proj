deea4e4ca455ddd78648342d91a077f5f1713ea7
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-02-29 18:27:30 -0700

    5

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const { auth } = require('express-openid-connect');
 
+const { auth } = require('express-openid-connect');
+
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -8,11 +10,11 @@ const config = {
   clientID: 'vRRJlC66eWClO5HvLe0DFzpFTCTpd255',
   issuerBaseURL: 'https://dev-gm1z4qm5wulqlarf.us.auth0.com'
 };
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+  
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  app.use(auth(config));
+  
+  // req.isAuthenticated is provided from the auth router
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
